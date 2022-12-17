7e2fc5470985b05fd243f67e0bd43aef664ddf05
oneku16 <eku.ulanov@yandex.com>
2022-12-18 01:40:19 +0600

    'models'

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Student(models.Model):
+
+    print_date = models.DateField(default=datetime.now().strftime("%Y-%m-%d"))
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    print_name = models.CharField(max_length=100)
+    print_cost = models.FloatField(default=50)
+    print_weight = models.FloatField(default=0)
+    is_academic = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['print_date']
+
+class Client(models.Model):
+
+    print_date = models.DateField(default=datetime.now().strftime("%Y-%m-%d"))
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    print_name = models.CharField(max_length=100)
+    print_cost = models.FloatField(default=50)
+    print_weight = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['print_date']
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Student, Client
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['POST'])
+def new_print(request, first_name, last_name, ):
+
+    if request.method == 'POST':
+        student_first_name = request.POST('student_name')
+        student_last_name = request.POST('student_name')
+        student = Student(student_first_name, student_last_name)
+
+        
